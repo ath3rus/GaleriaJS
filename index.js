56b6b8dbe83b4704028eb0ce3f8f5a1e08373f97
Roni <ronnyoliveira777@icloud.com>
2021-12-04 18:21:04 -0300

    final commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-function selecionar(event) {
-    alert(JSON.stringify(event))
-}
+const bgi = document.querySelector(".bgimg");
+const pqi = document.querySelectorAll(".pqimg");
+
+pqi.forEach (pqimg => pqimg.addEventListener("click", () => bgi.src = pqimg.src));
